34bab7c28248c50915befeee85bedeb980e129a7
Clodial <maravillamatthew@gmail.com>
2016-03-11 18:18:53 -0500

    test editing app.js

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req,res){
+		console.log("yo");
 		res.sendFile(__dirname + '/public/view/index.html');
 })
 
