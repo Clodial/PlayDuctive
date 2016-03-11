6203ee537ebdf5ab855eade7b854f6bda4ef1695
Clodial <maravillamatthew@gmail.com>
2016-03-11 17:14:20 -0500

    Edited app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // app.js
+
+// modules ======================================
+var express 		= require('express');
+var app				= express();
+var bodyParser		= require('body-parser');
+var methodOverride	= require('method-override');
+
+// configuration ===============================
+
+var port = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function(req,res){
+		res.sendFile(__dirname + '/public/view/index.html');
+})
+
+app.listen(8080);
+//
