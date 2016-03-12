181b7c3b0fa6c80c5ef5f5a62af81f9cd0fb87c3
Thomas Tu <tkt5@njit.edu>
2016-03-11 23:07:02 -0500

    empty commit
    pull requests are weird

--- sql/tabledefinitiontest.sql
+++ sql/tabledefinitiontest.sql
@@ -1,5 +1,6 @@
 INSERT INTO ClassTitles(classTitle) VALUES("testClass1"),("testClass2"),("testClass3");
 INSERT INTO Accounts(accountUser,accountPass,accountEmail) VALUES("testUser","testPass","testEmail");
 
+
 SELECT classId,accountUser,accountPass,classTitle,classExp FROM Classes,ClassTitles,Accounts WHERE 
 Classes.accountId=Accounts.accountId AND Classes.classTitleId=ClassTitles.classTitleId;
