67173ec4e85fb2e002f21bffc9477ed694110512
Clodial <maravillamatthew@gmail.com>
2016-03-13 13:38:24 -0400

    testing heroku issues

--- app.js
+++ app.js
@@ -3,21 +3,24 @@
 // modules ======================================
 var express 		= require('express');
 var app				= express();
+var logger 			= require('morgan');
 var bodyParser		= require('body-parser');
 var methodOverride	= require('method-override');
 
 // configuration ===============================
 
-var port = process.env.PORT || 8080;
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+//var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('*', function(req,res){
 		console.log("yo");
 		res.render('view/index.html');
 })
 
-app.listen(8080);
+//app.listen(8080);
+module.exports = app;
