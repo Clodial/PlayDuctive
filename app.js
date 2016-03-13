d57a1e7c090d9e971f424833a7a3ff082678e198
Clodial <maravillamatthew@gmail.com>
2016-03-13 14:04:25 -0400

    redoing app.js things

--- app.js
+++ app.js
@@ -3,24 +3,14 @@
 // modules ======================================
 var express 		= require('express');
 var app				= express();
+var adRouter		= express.Router();
 var logger 			= require('morgan');
 var bodyParser		= require('body-parser');
 var methodOverride	= require('method-override');
+var http			= require('http');
+var fs 				= require('fs');
 
 // configuration ===============================
 
-//var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/public'));
 
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.get('*', function(req,res){
-		console.log("yo");
-		res.render('view/index.html');
-})
-
-//app.listen(8080);
-module.exports = app;
