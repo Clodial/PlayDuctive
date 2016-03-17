80850114edbbfb90f13a9e12c8fa97c863e713af
Clodial <maravillamatthew@gmail.com>
2016-03-17 18:34:34 -0400

    editing app.js

--- app.js
+++ app.js
@@ -4,12 +4,7 @@
 var express 		= require('express');
 var app				= express();
 var adRouter		= express.Router();
-var logger 			= require('morgan');
-var bodyParser		= require('body-parser');
-var methodOverride	= require('method-override');
 var path 			= require('path');
-var http			= require('http');
-var fs 				= require('fs');
 
 // configuration ===============================
 
@@ -17,12 +12,14 @@ app.set('port', (process.env.PORT || 1337));
 
 app.use(express.static(__dirname + '/public'));
 
+app.get(function(req,res){
+	res.send("It Broke");
+});
 app.get('/', function(req,res){
 	res.sendFile(path.join(__dirname + '/public/view/index.html'));
 });
 
 
 app.listen(app.get('port'));
-console.log('at least this hopefully works');
 
 module.exports = app;
