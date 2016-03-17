921ed1d3dbff33b8282d27ff8f3efaf76467cc14
Clodial <maravillamatthew@gmail.com>
2016-03-17 19:02:20 -0400

    added path

--- app/routes/main-routes.js
+++ app/routes/main-routes.js
@@ -2,7 +2,8 @@
 console.log("yooo");
 var express = require('express');
 var router  = express();
+var path 	= require("path");
 
-app.get('/', function(req,res){
+app.get('/', function(req,res,next){
 	res.sendFile(path.join(__dirname + '/public/view/index.html'));
 });
