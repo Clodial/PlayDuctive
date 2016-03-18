ff6299a6b1ac2b7233a2d016fe63a0a627fa356d
Clodial <maravillamatthew@gmail.com>
2016-03-17 21:44:56 -0400

    routing js file now works

--- app.js
+++ app.js
@@ -4,17 +4,17 @@
 var express 		= require('express');
 var path			= require('path');
 var app				= express();
-var router 			= require('./app/routes/main-routes');
+var router			= require('./app/routes/main-routes');
 var users 			= require('./app/routes/main-users');
 
 // configuration ===============================
 
 app.set('port', (process.env.PORT || 1337));
 
-app.use(express.static(__dirname + '/public'));
-
 app.use('/', router);
 app.use('/users', users);
+
+//app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'));
 
--- app/routes/main-routes.js
+++ app/routes/main-routes.js
@@ -1,8 +1,9 @@
-//main-routes.js
 var express = require('express');
-var router  = express();
-var path 	= require("path");
+var router 	= express.Router();
+var path 	= require('path');
 
-app.get('/', function(req,res,next){
-	res.sendFile(path.join(__dirname + '/public/view/index.html'));
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '../../../public/view/index.html'));
 });
+
+module.exports = router;
