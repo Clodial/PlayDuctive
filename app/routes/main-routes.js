055a2f5d4aaa222d13f022f0ed06a544668264e6
Clodial <maravillamatthew@gmail.com>
2016-03-28 17:18:31 -0400

    fixing route

--- app/routes/main-routes.js
+++ app/routes/main-routes.js
@@ -52,7 +52,7 @@ router.post('/login/usetest', function(req,res){
             if (err) {
                 console.log(err.code);
             } else {
-                if(result.length > 0){console.log(solution)};
+                if(result.length > 0){console.log(result[0].solution);};
                 //res.send(resultNum);
             }
         }
