58a23749fe35a61a6f665a772a17caad35d880f4
Clodial <maravillamatthew@gmail.com>
2016-04-01 03:20:29 -0400

    ending session

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ app.use(function(req, res, next){
 	req.session.views 	= (req.session.views || 0) + 1;
 	//req.session.user 	= null;
 	//req.session.logIn 	= false;	
+	req.session.destroy();
 	next();
 
 });
