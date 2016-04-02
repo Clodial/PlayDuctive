bcfb3cf05af568df74b32aed29e7ad3414d7e3ad
Thomas Tu <tkt5@njit.edu>
2016-04-02 16:23:15 -0400

    added AccountsProjects table
    also added a creator for Projects and fixed up some MySQL syntax issues

--- sql/sqlFunctions.sql
+++ sql/sqlFunctions.sql
@@ -31,18 +31,5 @@ IN pass VARCHAR(255)
 BEGIN
 INSERT INTO Accounts(accountUser, accountPass, accountEmail)
 	VALUES user, email, pass;
-INSERT INTO Classes(accountId, classTitleId, classExp) SELECT Accounts.accountId, ClassTitles.classTitleId, 0 FROM Accounts, ClassTitles WHERE Accounts.accountUser = user 
-		AND ClassTitles.classTitle = "Dungeon Master";
-INSERT INTO Classes(accountId, classTitleId, classExp) SELECT Accounts.accountId, ClassTitles.classTitleId, 0 FROM Accounts, ClassTitles WHERE Accounts.accountUser = user 
-		AND ClassTitles.classTitle = "Warrior";
-INSERT INTO Classes(accountId, classTitleId, classExp) SELECT Accounts.accountId, ClassTitles.classTitleId, 0 FROM Accounts, ClassTitles WHERE Accounts.accountUser = user 
-		AND ClassTitles.classTitle = "Magician";
-INSERT INTO Classes(accountId, classTitleId, classExp) SELECT Accounts.accountId, ClassTitles.classTitleId, 0 FROM Accounts, ClassTitles WHERE Accounts.accountUser = user 
-		AND ClassTitles.classTitle = "Rogue";
-INSERT INTO Classes(accountId, classTitleId, classExp) SELECT Accounts.accountId, ClassTitles.classTitleId, 0 FROM Accounts, ClassTitles WHERE Accounts.accountUser = user 
-		AND ClassTitles.classTitle = "Tinkerer";
-INSERT INTO Classes(accountId, classTitleId, classExp) SELECT Accounts.accountId, ClassTitles.classTitleId, 0 FROM Accounts, ClassTitles WHERE Accounts.accountUser = user 
-		AND ClassTitles.classTitle = "Priest";
-
 END; //
 DELIMITER ;
