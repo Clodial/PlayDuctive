264757c207f36c90f5732d7b39ee619e782e4e5c
Thomas Tu <tkt5@njit.edu>
2016-04-02 22:53:32 -0400

    debug

--- app/components/create_project.js
+++ app/components/create_project.js
@@ -1,50 +1,53 @@
-$("button").click(function () {
-    $.ajax({
-        url: "makeProject/posts",
-        type: "POST",
-        dataType: "json",
-        data: JSON.stringify({
-            "accountName": "tkt5",
-            "accountPass": "5tkt",
-            "projType": $("[name=projType]").val(),
-            "projName": $("[name=projName]").val(),
-            "projDesc": $("[name=projDesc]").val()
-        }),
-        contentType: "application/json",
-        mimeType: "application/json",
-        success: function (result) {
-            //maybe do something to the page
-        }
+//attach listeners after DOM loaded
+$(function(){
+    $("button").click(function () {
+        $.ajax({
+            url: "makeProject/posts",
+            type: "POST",
+            dataType: "json",
+            data: JSON.stringify({
+                "accountName": "tkt5",
+                "accountPass": "5tkt",
+                "projType": $("[name=projType]").val(),
+                "projName": $("[name=projName]").val(),
+                "projDesc": $("[name=projDesc]").val()
+            }),
+            contentType: "application/json",
+            mimeType: "application/json",
+            success: function (result) {
+                //maybe do something to the page
+            }
+        });
     });
-});
 
-//Function checking for users
-$("#addUser").keyup(function(){
-    console.log("keyup");/*
-    var userPartial = document.getElementById("addUser").value;
-    var useCheck = "";
-    
-    $.ajax({
-        url: "/makeProject/search_users",
-        type: "POST",
-        datatype: "json",
-        data: JSON.stringify({
-            "userPartial": userPartial
-        }),
-        contentType: "application/json",
-        mimeType: "application/json",
-        success: function(data){
-            //clear the current list
-            var x = document.getElementById("userSelect");
-            while (x.length > 0) {
-                x.remove(0);
+    //Function checking for users
+    $("body").keyup("addUser",function(){
+        console.log("keyup");/*
+        var userPartial = document.getElementById("addUser").value;
+        var useCheck = "";
+        
+        $.ajax({
+            url: "/makeProject/search_users",
+            type: "POST",
+            datatype: "json",
+            data: JSON.stringify({
+                "userPartial": userPartial
+            }),
+            contentType: "application/json",
+            mimeType: "application/json",
+            success: function(data){
+                //clear the current list
+                var x = document.getElementById("userSelect");
+                while (x.length > 0) {
+                    x.remove(0);
+                }
+                //add the new elements
+                for(i=0;i<data.length;i++) {
+                    var option=document.createElement("option");
+                    option.text=data[i];
+                    x.add(option);
+                }
             }
-            //add the new elements
-            for(i=0;i<data.length;i++) {
-                var option=document.createElement("option");
-                option.text=data[i];
-                x.add(option);
-            }
-        }
-    });*/
+        });*/
+    });
 });
