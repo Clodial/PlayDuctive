bb5945a95cc82f7c71f6c1f26fd2d777231ed44b
Clodial <maravillamatthew@gmail.com>
2016-04-04 00:48:04 -0400

    testing data

--- app/routes/main-routes.js
+++ app/routes/main-routes.js
@@ -23,7 +23,7 @@ router.get('/', function(req,res){
         con.query('CALL getProjects(?);', [req.session.user],
             function(err, result){
                 for(var i = 0; i < result.length; i++){
-                    console.log(result[0].name);
+                    console.log(result[0]);
                     console.log(result[0].status);
                     projList.push([result[0].name, result[0].status]);
                 }
